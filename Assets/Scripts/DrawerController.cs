b65b9e3bb006f5c038b9d0638325eee5a544bf92
nh100 <noahh717@gmail.com>
2015-04-19 10:42:03 -0400

    Added shit

---
+++ Assets/Scripts/DrawerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
+
+public class DrawerController : MonoBehaviour {
+
+    Vector3 lastPosition;
+    List<Vector3> vectors = new List<Vector3>();
+    const float mulFactor = 0.7f;
+    const float accelFactor = 40.0f;
+    public GameObject myo;
+    public GameObject system = null;
+    public static Material lineMaterial;
+	void Start () {
+       
+	}
+
+    static void CreateLineMaterial()
+    {
+        if (!lineMaterial)
+        {
+            lineMaterial = new Material("Shader \"Lines/Colored Blended\" {" + "SubShader { Pass { " + "    Blend SrcAlpha OneMinusSrcAlpha " + "    ZWrite Off Cull Off Fog { Mode Off } " + "    BindChannels {" + "      Bind \"vertex\", vertex Bind \"color\", color }" + "} } }");
+            lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+            lineMaterial.shader.hideFlags = HideFlags.HideAndDontSave;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+
+        ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
+        Vector3 acc = thalmicMyo.accelerometer;
+        Debug.Log("X: " + thalmicMyo.accelerometer.x);
+        transform.position = new Vector3((transform.position.y + (Mathf.Pow(acc.y, 3))) * 1.0f/3.0f * Time.deltaTime * accelFactor,
+            (transform.position.x + (Mathf.Pow(acc.x, 3) * 70)) * 1.0f/3.0f * Time.deltaTime, 0);
+        ParticleSystem particles = system.GetComponent<ParticleSystem>();
+        if (thalmicMyo.pose == Pose.Fist)
+        {   
+            particles.emissionRate = 2500.0f;
+        }
+        else
+        {
+            particles.emissionRate = 60.0f;
+        }
+        if (thalmicMyo.pose == Pose.WaveIn)
+        {
+            particles.startColor = Color.cyan;
+        }
+        else if (thalmicMyo.pose == Pose.WaveOut)
+        {
+            particles.startColor = Color.yellow;
+        }
+        else
+        {
+            particles.startColor = Color.grey;
+        }
+	}
+}
---
+++ Assets/Scripts/Startup.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Startup : MonoBehaviour {
+
+    public GameObject myo = null;
+    
+	// Use this for initialization
+	void Start () {
+        ThalmicHub hub = ThalmicHub.instance;
+        ThalmicMyo myoInstance = myo.GetComponent<ThalmicMyo>();
+
+        if (!hub.hubInitialized)
+        {
+            Debug.Log("Cannot connect Myo Connect.");
+        }
+        else if (!myoInstance.isPaired)
+        {
+            Debug.Log("Myo is not paired.");
+        }
+        else if (!myoInstance.armSynced)
+        {
+            Debug.Log("Myo not synced.");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ThalmicHub hub = ThalmicHub.instance;
+        if (Input.GetKeyDown(KeyCode.E))
+            hub.ResetHub();
+	}
+}
